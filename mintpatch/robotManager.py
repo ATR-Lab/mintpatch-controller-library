615be8cb785acb4ab6f798dcb5f9ef2461c1892b
nathanmoder <63596741+nathanmoder@users.noreply.github.com>
2021-03-29 13:25:41 -0400

    Commented and improved constructor

--- mintpatch/robotManager.py
+++ mintpatch/robotManager.py
@@ -3,7 +3,7 @@ Creates the Servo Manager, which will store information about
 which port each servo is attached to.
 It also handles the startup of MintPatch.
 Nathan Moder
-3/23/2021
+3/29/2021
 """
 
 from portManager import PortManager
@@ -16,13 +16,14 @@ class RobotManager:
     After execution, the manager and all port managers will be ready to function.
     TODO: Implement code to get rid of empty portManagers, if we'll be checking every port of the appropriate type"""
     def __init__(self, _port_list, _setup_info):
-        #defines empty arrays
+        
+        """
+        Defines empty arrays
+        """
+
         self.servo_list=[] #array of pairs of (port_name(string), servo_id(int))
         self.ports_by_name={} #a dictionary that connects the PortManager objects to strings of their names
-        self.port_names=_port_list #string array
-        
-        #TODO: temp list may be outdated. revise
-        temp_port_list={}
+        self.port_names=_port_list #string array of the paths to the ports
         
 
         """
@@ -30,38 +31,50 @@ class RobotManager:
         port manager. then, it makes not of every servo attached to it.
         """
 
-        #TODO: variables outside of loops is outdated.
-        # update and make more condusive to python norms
-        i=0
-        j=0
-        for item in _port_list:
-           
-            #debug print
-            #print(item) 
-           
-            a=PortManager(item,_setup_info[item])
-            temp_port_list[i]=a
-            self.ports_by_name[a.port_name]=a
 
-            for servo in temp_port_list[i].servos:
-            
-                self.servo_list.append((temp_port_list[i].port_name, servo))
-                j=j+1
-            
-            i=i+1
+        for portn in _port_list:
+            #Enter the loop for every port name.
+            temp_manager=PortManager(portn,_setup_info[portn])
+
+            if not temp_manager.is_empty():
+                #We want to make sure we aren't storing empty ports.
+
+                self.ports_by_name[portn]=temp_manager
+                    #For this port, a Port Manager is set up.
+                        #Since _setup_info is indexed by port name, we simply index it
+                        #The same name will is used to index ports_by_name 
+                                   
+            else:
+                continue
+                #If it is empty, we have no need to go into the other loop.
+
+            for servo in self.ports_by_name[portn].servos:
+                #Utilizes and looks through the list of servos in the port manager.
+
+                self.servo_list.append((portn, servo))
+                    #Stores a pair of values in the servo_list.
+                        #We want it to seperately track the port and servo ID for later ease.
+
+
+
+    """
+    Simple seach through the servo list to see if it exists.
+    Could be used for preventing errors with faulty indexes
+    and tool calls.
+    Returns a boolean.
+    """
+    def check_included(self, pname, sid):
+        #Uses itterate and keep track algorithm
+        found=False
+
+        for pair in self.servo_list:
         
-
-    """
-    simple seach through the servo list to see if it exists.
-    could be used for map security
-    TODO: Class structure has changed since this itteration. update
-    """
-    def check_included(self, _query_servo):
-        in_manager=False    
-        for servo in self.servo_list:
-            if servo==_query_servo:
-                in_manager=True
-        return in_manager
+            if pair[0]==pname and pair[1]==sid:
+                found=True
+            #As this is a pair of port name and servo ID, we check both to see
+            #if we found our matching servo.
+       
+        return found
     
     #returns the string list of every servo's name
     def list_servos(self):
@@ -73,27 +86,44 @@ class RobotManager:
     """
     goes through every servo and gets info
     uses PortManager and Wrapper
-    TODO: Debug for real servos
     returns an array of dictionaries
     """
     def list_all_servo_info(self):
 
         info_dict=[]
+            #Defines the empty array which will hold the dictionaries
         
         for portn in self.port_names:
-            #stores the PortManager in port
             port=self.ports_by_name[portn]
-        
-            for ids in range(0,port.servos.__len__()):
-                
-                temp_dict=port.wrapper.get_feedback(port.servos[ids])
-                temp_dict['id']='{portn}_{id}'.format(portn=portn,id=port.servos[ids])
+            #Loops through every Port Manager using their name,
+            #since that is how they are stored
+
+            for ids in port.servos:
+                #This loop goes through the servos on the port. 
+                    #ids is an integer number that corresponds
+                    #to the value in a register on the motor.
+
+                temp_dict=port.wrapper.get_feedback(ids)
+                    #Accesses the wrapper held by the port manager.
+                        #From it, it uses get_feedback to obtain a comprehensive
+                        #list of dynamic information from the servo.
+                            #TODO: Add somewhere a means of obtaining static info as well.
+
+                temp_dict['id']='{portn}_{id}'.format(portn=portn,id=ids)
+                    #The ID returned only describes the motor, but we need to
+                    #provide the port name for it to be useful to outside systems.
+                        #We use the format function to create our new Servo Name:
+                            #<port_name>_<motor_id>
+                            #For example, for motor 5 on the port "USB0", we would have:
+                                #USB0_5
+                        #TODO: Must currently be adapted if we want double-digit motor ID's
+
                 info_dict.append(temp_dict)
+                    #We add the obtained and edited dictionary to the list we will return.
 
         return info_dict
 
     #Returns an array of the names of every port.
-    #Does not return repeats.
-    #TODO: Debug
     def list_ports(self):
         return self.port_names
+
