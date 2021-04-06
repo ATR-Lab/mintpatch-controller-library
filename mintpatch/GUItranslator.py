63cb003bfb36d8a0bc092d7cfe7966bc1146b378
nathanmoder <63596741+nathanmoder@users.noreply.github.com>
2021-04-06 14:41:25 -0400

    GUI translator now logs info

--- mintpatch/GUItranslator.py
+++ mintpatch/GUItranslator.py
@@ -1,15 +1,43 @@
 """
 Responsible for communication between the GUI and library.
 Currently reads from JSON and outputs to consule.
-Legacy version uses a python consule.
-Original Author: Nathan Moder
-Modified and cleaned up by: M. Arnett
-3/29/2021
+Test version uses a python consule.
+Author: Nathan Moder
+Assistance: M. Arnett
+4/6/2021
 """
 
 
 # Imports for interaction with Node.js
 import sys, json
+
+
+class ServoLog:
+    def __init__(self,servo_name):
+        self.output_name=servo_name
+        self.info_dictionary={}
+
+    def log_servo(self, info_dictionary):
+        self.info_dictionary=info_dictionary
+
+    def print_servo(self):
+        # Order to be printed:
+        # id, stateID, voltage, temperature, angle, speed
+        # The dictionary has a boolean value declaring whether the motor is moving.
+
+        #stored to a temporary value for readability
+        idict=self.info_dictionary
+        if idict['moving']:
+            state = 'RUNNING'
+        
+        else:
+            state = 'IDLE'
+        
+        # Prints the info out in one line as a formatted string.
+        # Every element is seperated by a space.
+        print('{id} {state} {voltage} {temp} {pos} {speed}'.format(id = self.output_name, 
+        state = state, voltage = idict["voltage"], temp = idict["temperature"], 
+        pos = idict["position"]*.088, speed = idict["speed"]))
 
 
 # Uses funcitons from RobotManager and the wrapper, but does not need to import them.
@@ -27,6 +55,11 @@ class GUITranslator:
         # <portname>_<motorID>
         self.running_motors = []
 
+        # A list of ServoLogs, indexed by servo names(formatted as above).
+        # It is used to speed up gathering information, by
+        # keeping the data from unchanging motors.
+        self.log_list={}
+
 
     def read_from_json(self):
         """
@@ -41,146 +74,124 @@ class GUITranslator:
         # loads() does the translation
         return json.loads(jin)
 
-
-    def print_from_dict(self, idict):
-        """
-        An internal functions for readability. 
-        Prints the info out of a Dictionary from the wrapper.
-        Output, but no returns.
-        """
-        # idict is the information dictionary from a single motor.
-        # Order to be printed:
-        # id, stateID, voltage, temperature, angle, speed
-        # The dictionary has a boolean value declaring whether the motor is moving.
-        if idict['moving']:
-            state = 'RUNNING'
-        
-        else:
-            state = 'IDLE'
-        
-        # Prints the info out in one line as a formatted string.
-        # Every element is seperated by a space.
-        print('{id} {state} {voltage} {temp} {pos} {speed}'.format(id = idict["id"], 
-        state = state, voltage = idict["voltage"], temp = idict["temperature"], 
-        pos = {idict["position"]}, speed = idict["speed"]))    
-        
     
-    def stop_motor(self,servo_name):
-        """
-        Halts the movement of the named servo
-        No returns or output
-        """
-        # These two variables can be adjusted if we allow 2-digit or 3-digit motor ID's.
-        # The name of the port is the first part of the string.
-        # We use python string operations to remove the last 2 characters.
-        pname = servo_name[:-4]
-
-        # The ID of the motor is just the last element.
-        sid = int(servo_name[-3:])
-
-        # Attepting to stop a motor that doesn't exist would be bad. Many errors.
-        if self.manager.check_included(pname,sid):
-            # Disable torque when stopping the motor
-            self.manager.ports_by_name[pname].proxy.set_torque_enabled(sid, [0])
-
-            # Uses a function in the wrapper to set the goal velocity to zero. 
-            # This will slow the motor to a stop.
-            # ports_by_name is indexed by the port name, 
-            # while the wrapper function needs the motor ID.
-            self.manager.ports_by_name[pname].proxy.set_goal_position(sid,0)
-
-            # Removes the name from the internal list. 
-            # It is no longer known to be running.
-            self.running_motors.remove(servo_name)
-                
-        
-        # This path depends on our safety priorities.
-        # TODO: Determine if error correction will cause latency issues.
-        """
-        else:
-            #if a servo is entered that doesn't seem to exist, we assume
-            #that the typo was dangerous and stop the whole system
-            self.stop_all_motors()
-        """
-
-
-    def stop_all_motors(self):
-        """
-        Halts all movement in the system.
-        No returns or output.
-        """
-        # We loop though ever motor that we know to be running, and stop them.
-        for motor in self.running_motors:
-            self.stop_motor(motor)
-
-
-    def move_motor(self, servo_name, speed):
+    def move_motor(self, consule_input, input_length):
         """
         Function member will enable torque, and then change the goal position,
         which then appends the servo to a list of running motors.
         Returns nothing.
         """
+        
+        #If we don't have a motor and a position, this is an error
+        if input_length != 3:
+            return True
+
+        #extract the needed name from the input
+        servo_name=consule_input[1]
+        
         #These values hold the same role as in stop_motor.
         pname = servo_name[:-4]
         sid = int(servo_name[-3:])
-
+        
         # Trying to move a motor not attached to the system would be 
         # just as bad as stopping one.
         if self.manager.check_included(pname,sid):
+
             # Enable torque for motor to have it set goal position.
             self.manager.ports_by_name[pname].proxy.set_torque_enabled(sid, [1])
 
             # Set goal position of the motor
-            self.manager.ports_by_name[pname].proxy.set_goal_position(sid,int(speed))
+            self.manager.ports_by_name[pname].proxy.set_goal_position(sid,int(consule_input[2]*(1000/88)))
 
             # We add the motor to a list of currently running motors.
             self.running_motors.append(servo_name)
+        return True
 
 
     def update_motor(self, servo_name):
         """
-        Prints the full information set for one named servo.
-        Output, but no returns.
+        Stores the full information set for one named servo
+        into the appropriate Log.
+        No output or returns.
         """
-        # These values hold the same role as in stop_motor.
+        
+        # The port name and motor ID, extracted from the full servo name.
+        # Used to navigate the architecture to access the proxy/wrapper.
         pname = servo_name[:-4]
-        sid = int(servo_name[-3:])
+        mid = int(servo_name[-3:])
 
         # We cannot try to access a motor which isn't attached.
-        if self.manager.check_included(pname, sid):
+        if self.manager.check_included(pname, mid):
         
             # Defines an empty set which will store the information dictionary.
             idict = {}
         
             # Uses the wrapper to fill that dictionary with the needed entries.
-            idict = self.manager.ports_by_name[pname].proxy.get_feedback(sid)
+            idict = self.manager.ports_by_name[pname].proxy.get_feedback(mid)
         
-            # The motor ID in the dictionary does not include the port.
-            # Since we want to output a combined name, we alter the entry.
-            idict['id'] = servo_name
-
-            # Since this was a dictionary returned from get_feedback, we can output it with
-            # this internal function.
-            self.print_from_dict(idict)
+            # Since the log list is indexed by servo name, we add the information here to the Log
+            self.log_list[servo_name].log_servo(idict)
 
         else:
             print("No such servo")
         
 
-    def update_all(self):
+    def scan(self, consule_input, input_length):
         """
-        Function for updating the all the information on every servo motor.
+        Gathers information on every motor attached to the system,
+        setting up the Logs. It *must* be run in order for
+        future updates to work.
         """
+
         # Since the RobotManager has the data structures which are needed
         # to access every servo, the data collection is handled by that class.
         # This function returns an array of dictionaries.
         all_info = self.manager.list_all_servo_info()
         
-        # We loop through all the individual dictionaries, and print them out.
+        # We loop through all the individual dictionaries, and create a servo log for each.
+        # This will give us a data structure of every servo attached to the computer, and
+        # prepare them to be updated only as needed.
         for lmotor in all_info:
-            self.print_from_dict(lmotor)
 
-    
+            # Creates the Log, giving it its name and index.
+            # if the Log already exists, no errors nor bugs will occur.
+            # It just overwrites what's already there.
+            self.log_list[lmotor['id']]=ServoLog(lmotor['id'])
+            
+            #The information in the dictionary is passed to the Log.
+            self.log_list[lmotor['id']].log_servo(lmotor)
+
+            #The Log prints all of its information.
+            self.log_list[lmotor['id']].print_servo()
+        
+        return True
+
+
+    def running_update(self, consule_input, input_length):
+        
+        # Uses the Drivers to update *only* the motors
+        # which are running, and therefore changing.
+        for rmotor in self.running_motors:
+            self.update_motor(rmotor)
+
+        #Prints out every motor.
+        for servo_log in self.log_list:
+            self.log_list[servo_log].print_servo()
+        
+        return True
+
+
+    def end_listening(self, consule_input, input_length):
+        #Just returns false, causing the listener to exit its loop
+        # and the program to end.
+        return False
+
+
+    def ignore(self, consule_input, input_length):
+        #Used to prevent errors in the switch.
+        return True
+
+
     def listen(self):
         """
         This is the function that MintPatch will be in while idle.
@@ -188,18 +199,32 @@ class GUITranslator:
         However, the main purpose of this function is to parse input and
         call the appropriate functions to handle the command cases.
         """
-        
+
+        # This dictionary will be used as a switch statement
+        # to quickly parse input.
+        # It translates strings into the appropriate function,
+        # Each of which has the same parameters and return.
+        switch_dict={
+            'move': self.move_motor,
+            'scan': self.scan,
+            'update': self.running_update,
+            'end' : self.end_listening
+        }
+
+
         # LOOP SETUP
         Continue=True
 
         # Establishes a common, potentially infinite while loop.
         # It can be ended by inputing "end".
         while(Continue):
+            
             # GATHERING INPUT
+           
             # NODE.JS
             # Uses this input function to gather one line from the Node.
             # This is a string. 
-            # TODO: Implement this possibly
+            
             #conin = self.read_from_json()        
             
             # CONSOLE
@@ -208,71 +233,35 @@ class GUITranslator:
             # INPUT SETUP
             # Since it's a string, and the parts of the input should be seperated
             # by spaces; we split this to make it an array.
+            
             cinar = conin.split()
             
             # Knowing the length of this string array will
             # let us catch errors and quickly skip faulty commands.
             l = cinar.__len__()
 
-            # HANDLE DIFFERENT COMMANDS
             # In this case, there is no input.
             # There's no reason to go through the loop.
             if l == 0:
                 continue
 
-            # In this case, we know we will be stopping motors.
-            if cinar[0] == "stop":
-                # If the word is alone, we assume this is an emergency stop.
-                if l == 1:
-                    self.stop_all_motors()
 
-                # Otherwise, we let the user specify a single motor to stop.
-                else:
-                    # If they try to specify more than one motor, the format is wrong.
-                    if l != 2:
-                        continue
+            # SWITCH
 
-                    # Correctly called, we stop the motor.
-                    self.stop_motor(cinar[1])
+            # This is the magical switch statement. It runs the first
+            # word of the input through the switch dictionary,
+            # finding the appropriate function to run. Then, it
+            # passes the full input array and its length. Not every
+            # function uses them, but this allows for a very efficient call.
 
-                continue
+            # In the case that the input is faulty, the switch will call the
+            # "ignore" function. This does nothing.
 
-            if cinar[0] == "move":
-                # We need both a servo and a speed, and nothing else.
-                if l != 3:
-                    continue
-
-                self.move_motor(cinar[1], cinar[2])
-                continue
-
-
-            if cinar[0] == "update":
-                if l == 1: 
-                   continue
-
-                self.update_motor(cinar[1])
-                continue
-
-            # Updates every running motor.
-            if cinar[0] == "running":
-                # Uses the update_motor function on every motor in the array.
-                for motor in self.running_motors:
-                    self.update_motor(motor)
-
-                continue
+            # Finally, each function returns a Boolean telling the listener
+            # whether or not to continue the loop. Every function except "end"
+            # returns true.
+            Continue=switch_dict.get(cinar[0], self.ignore)(cinar,l)
             
-            if cinar[0] == "list":
-                for string in self.manager.list_servos():
-                    print(string)
-
-                continue
-
-            if cinar[0] == "scan":
-                self.update_all()
-                continue
-
-            if cinar[0] == "end":
-                Continue = False
 
         # END LOOP
         print("Thank you for using MintPatch!")
