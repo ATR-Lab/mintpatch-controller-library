95fb4d8a7fe2b38e811bf1766e26d686ff68bc15
nathanmoder <63596741+nathanmoder@users.noreply.github.com>
2021-04-06 21:42:09 -0400

    Update startup.py

--- mintpatch/startup.py
+++ mintpatch/startup.py
@@ -1,40 +1,48 @@
 """
 The startup code for MintPatch.
-Original Author: Nathan Moder
-Cleaned up by: M. Arnett
-3/29/2021
+Temporarily, there is no input.
+TODO: There will eventually be dynamic startup.
+#Nathan Moder
+#3/29/2021
 """
 
-
-# Imports from custom classes
+from emulatedMotor import EmulatedMotor
 from GUItranslator import GUITranslator
 from robotManager import RobotManager
-from emulatedMotor import EmulatedMotor
-
-# Import standard libraries
 import json, sys
 
 
 def main():
-    # Setttings for configuring our pinging to the motors.
-    settings_set = {}
-
-    # Real motors, and matches the launch file simple_l_arm_controller_manager.launch
-    port_list = {'ttyUSB0'}
-    settings = {
+    settings_set={}
+    
+    #This one works with the emulated servos included in the directory
+    #"""
+    port_list={'port_1','port_2'}
+    settings={
+        'baudrate': 100,
+        'minID': 1,
+        'maxID': 50,
+        'updateRate' : 100,
+        'diagnosticsRate' : 100
+    }
+    settings_set['port_1']=settings
+    settings_set['port_2']=settings
+    """
+    #This one is real, and matches the launch file simple_l_arm_controller_manager.launch
+    port_list={'ttyUSB0'}
+    settings={
         'baudrate': 1000000,
         'minID': 1,
         'maxID': 40,
         'updateRate' : 20,
         'diagnosticsRate' : 1
     }
-    settings_set['ttyUSB0'] = settings
+    settings_set['ttyUSB0']=settings
+    """
 
-    # Making robot manager, and basically listening in on the GUI translator
-    manager = RobotManager(port_list, settings_set)
-    translator = GUITranslator(manager)
+    manager=RobotManager(port_list,settings_set)
+    translator=GUITranslator(manager)
     translator.listen()
-
 
 if __name__ == '__main__':
     main()
